2564a40af3a427f9c86d71a51803bf3656bda458
Pawan Singh <almightycoder4@gmail.com>
2023-03-09 20:13:22 +0530

    navbar active link fix

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@
 let sections = document.querySelectorAll("section");
 let navLinks = document.querySelectorAll("header nav a");
 window.onscroll = () => {
-  console.log("hiii");
   sections.forEach((sec) => {
     let top = window.scrollY;
     let offset = sec.offsetTop - 150;
@@ -10,18 +9,15 @@ window.onscroll = () => {
     let id = sec.getAttribute("id");
 
     if (top >= offset && top < offset + height) {
-      console.log("yes");
       navLinks.forEach((links) => {
         links.classList.remove("active");
         document
           .querySelector("header nav a[href*=" + id + "]")
-          .classList.add("active").add;
+          .classList.add("active");
       });
     }
   });
-};
-// sticky navbar//
-window.onscroll = () => {
-  let header = document.querySelector("header");
+  // sticky navbar//
+  let header = document.querySelector(".header");
   header.classList.toggle("sticky", window.scrollY > 100);
 };
