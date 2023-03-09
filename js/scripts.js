d9f97bd2f8bc89c4e5948929fc542495aa0d2732
Pawan Singh <almightycoder4@gmail.com>
2023-03-10 02:35:17 +0530

    Porfolio rotation animantion done

--- js/scripts.js
+++ js/scripts.js
@@ -33,3 +33,29 @@ menuIcon.onclick = () => {
   menuIcon.classList.toggle("bx-x");
   navbar.classList.toggle("active");
 };
+
+// ===========dark light mode==========//
+let darkModeIcon = document.querySelector("#darkMode-icon");
+
+darkModeIcon.onclick = () => {
+  darkModeIcon.classList.toggle("bxs-sun");
+  document.body.classList.toggle("dark-mode");
+};
+
+// ===========Scroll reveal==========//
+ScrollReveal({
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".home-content,.heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img img,.services-container,.portfolio-box,.contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1,.about-img img", { origin: "left" });
+ScrollReveal().reveal(".home-content h3,.home-content p,.about-content", {
+  origin: "right",
+});
