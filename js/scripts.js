fb4db75820ff6a8822fad42b110a0a5f78fafb2d
Pawan Singh <almightycoder4@gmail.com>
2023-03-12 23:46:08 +0530

    portfolio some changes added

--- js/scripts.js
+++ js/scripts.js
@@ -59,3 +59,4 @@ ScrollReveal().reveal(".home-content h1,.about-img img", { origin: "left" });
 ScrollReveal().reveal(".home-content h3,.home-content p,.about-content", {
   origin: "right",
 });
+// ===========Scroll reveal==========//
